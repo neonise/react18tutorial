f91518bbc4c9256b70750e05bbfaa36fff1a93df
Nosha <Nosha@DESKTOP-HCLS1DA>
2023-03-17 11:42:45 +0330

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function Greeting() {
-    return <h2>Hello I'm a component</h2>
-}
+const books = [
+  {
+    image: "./images/book.jpg",
+    author: "James Cameron",
+    title: "Atomic Hobits",
+    id: 1,
+  },
+  {
+    image: "./images/book.jpg",
+    author: "Nik Red",
+    title: "Read Me!",
+    id: 2,
+  },
+  {
+    image: "./images/book.jpg",
+    author: "Barbara De Anglis",
+    title: "My Travels",
+    id: 3,
+  },
+];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book book={book} key={book.id} />;
+      })}
+    </section>
+  );
+};
 
-root.render(<Greeting/>)
+const Book = (props) => {
+  const book = props.book;
+  return (
+    <article className="book">
+      <img src={book.image} />
+      <h2>{book.title}</h2>
+      <h4>{book.author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
