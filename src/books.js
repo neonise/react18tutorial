7bf9c9fa5dde491f6186337f74e63635a5a0e2c4
Nosha <Nosha@DESKTOP-HCLS1DA>
2023-03-18 11:29:20 +0330

    init

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import bookimg from "./images/book.jpg";
+import littleimg from "./images/little.jpg";
+
+export const books = [
+  {
+    image: bookimg,
+    author: "James Cameron",
+    title: "Atomic Hobits",
+    id: 1,
+  },
+  {
+    image: littleimg,
+    author: "Nik Red",
+    title: "Read Me!",
+    id: 2,
+  },
+  {
+    image: bookimg,
+    author: "Barbara De Anglis",
+    title: "My Travels",
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,46 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Book from "./Book";
+import { books } from "./books";
 import "./index.css";
-
-const books = [
-  {
-    image: "./images/book.jpg",
-    author: "James Cameron",
-    title: "Atomic Hobits",
-    id: 1,
-  },
-  {
-    image: "./images/book.jpg",
-    author: "Nik Red",
-    title: "Read Me!",
-    id: 2,
-  },
-  {
-    image: "./images/book.jpg",
-    author: "Barbara De Anglis",
-    title: "My Travels",
-    id: 3,
-  },
-];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return <Book book={book} key={book.id} />;
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const book = props.book;
-  return (
-    <article className="book">
-      <img src={book.image} />
-      <h2>{book.title}</h2>
-      <h4>{book.author}</h4>
-    </article>
   );
 };
 
