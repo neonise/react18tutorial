9710f40c73f30767795a042e94cf9fc65d10a8b2
Nosha <Nosha@DESKTOP-HCLS1DA>
2023-03-19 18:17:50 +0330

    advance

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import Book from "./Book";
-import { books } from "./books";
-import "./index.css";
 
-const BookList = () => {
+// import "./index.css";
+
+const Comp = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <section className="booklist">
-      {books.map((book, index) => {
-        return <Book {...book} key={book.id} number={index} />;
-      })}
-    </section>
+    <>
+      <div>
+        <h4>You clicked {count} times!</h4>
+        <button type="button" onClick={handleClick}>
+          Click
+        </button>
+      </div>
+    </>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<BookList />);
+root.render(<Comp />);
